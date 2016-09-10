685e5f7b479f79483064362cff65791f76ff50d3
grinnelldennis <chanchun@grinnell.edu>
2016-09-10 09:25:33 -0500

    Added extra credit- focus letter!

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -33,6 +33,80 @@ public class SpeedReader {
 	    }
 	}
 
+	
+	public static void demonstratePanelOffset(WordGenerator w, int width, int height, int fontSize, int wpm) throws InterruptedException {
+	    DrawingPanel panel = new DrawingPanel(width, height);
+	    Font f = new Font("Courier", Font.BOLD, fontSize);
+	    Graphics g = panel.getGraphics();
+	    FontMetrics fm = g.getFontMetrics(new Font("Courier", Font.BOLD, fontSize));
+
+	    g.setFont(f);
+	    
+	    while (w.hasNext()){
+	    	String s = w.next();
+	    	int sW = fm.stringWidth(s);
+	    	
+	    	focusLetter(g, s, width, height, wpm, sW);
+	    }
+	}
+	
+	public static void focusLetter (Graphics g, String s, int width, int height, int wpm, int sW) throws InterruptedException {
+		int stringLength = s.length();
+		int charWidth = sW / stringLength; 
+		char c = s.charAt( stringLength-1 );
+		
+		if ( stringLength < 2 ){
+	    	g.setColor(Color.red); 
+	    	g.drawString(s.substring(0), (width/2)-(charWidth/2), height/2-20);	
+	    	
+		} else if ( stringLength < 6 ){
+			g.setColor(Color.black);
+	    	g.drawString(s.substring(0, 1), (width/2)-charWidth, height/2-20);
+	    	g.setColor(Color.red);
+	    	g.drawString(s.substring(1, 2), (width/2), height/2-20);
+	    	
+	    	if (stringLength > 2){
+	    		g.setColor(Color.black);
+		    	g.drawString(s.substring(2), (width/2)+charWidth, height/2-20);	
+	    	}
+		} else if ( stringLength < 10 ){
+			g.setColor(Color.black);
+	    	g.drawString(s.substring(0, 2), (width/2)-charWidth*2, height/2-20);	
+	    	g.setColor(Color.red);
+	    	g.drawString(s.substring(2, 3), (width/2), height/2-20);
+	    	g.setColor(Color.black);
+	    	g.drawString(s.substring(3), (width/2)+charWidth, height/2-20);	
+	    	
+		} else if ( stringLength < 13 ){
+			g.setColor(Color.black);
+	    	g.drawString(s.substring(0, 3), (width/2)-charWidth*3, height/2-20);	
+	    	g.setColor(Color.red);
+	    	g.drawString(s.substring(3, 4), (width/2), height/2-20);
+	    	g.setColor(Color.black);
+	    	g.drawString(s.substring(4), (width/2)+charWidth, height/2-20);	
+	    	
+		} else {
+			g.setColor(Color.black);
+	    	g.drawString(s.substring(0, 4), (width/2)-charWidth*4, height/2-20);	
+	    	g.setColor(Color.red);
+	    	g.drawString(s.substring(4, 5), (width/2), height/2-20);
+	    	g.setColor(Color.black);
+	    	g.drawString(s.substring(5), (width/2)+charWidth, height/2-20);	
+	    	
+		}
+		
+		if ( c == '.' || c == '!' || c == '?' || c == ','){
+			Thread.sleep((long) (60000/wpm*1.75));
+		}
+		else {
+			Thread.sleep(60000/wpm);
+		}
+		
+		g.setColor(Color.white);
+    	g.fillRect(0, 0, width, height);
+	}
+	
+
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		// TODO Auto-generated method stub
 
