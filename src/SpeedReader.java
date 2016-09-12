4d8b88be9df52c8822347805127f9404e8c45903
grinnelldennis <chanchun@grinnell.edu>
2016-09-12 16:15:09 -0500

    Made changes to conform with style guide

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -1,16 +1,22 @@
-//Speed
 import java.awt.Font;
 import java.awt.Graphics;
 import java.io.FileNotFoundException;
 import java.awt.*;
 
-/* 
- * https://www.mkyong.com/java/java-convert-string-to-int/
- */
-
 public class SpeedReader {
 	
-	public static void demonstratePanelCenter(WordGenerator w, int width, int height, int fontSize, int wpm) throws InterruptedException {
+	/**
+	 *  Centers each word displayed
+	 * @param w
+	 * @param width
+	 * @param height
+	 * @param fontSize
+	 * @param wpm
+	 * @throws InterruptedException
+	 */
+	public static void demonstratePanelCenter(WordGenerator w, int width, int height, int fontSize, 
+			int wpm) throws InterruptedException {
+		
 	    DrawingPanel panel = new DrawingPanel(width, height);
 	    Font f = new Font("Courier", Font.BOLD, fontSize);
 	    Graphics g = panel.getGraphics();
@@ -24,17 +30,27 @@ public class SpeedReader {
 	    	int sW = fm.stringWidth(s);
 	    	
 	    	g.setColor(Color.black);
-	    	g.drawString(s, (width/2)-(sW/2), height/2-20);	
+	    	g.drawString(s, (width / 2) - (sW / 2), (height / 2) - 20);	
 	    	
-	    	Thread.sleep(60000/wpm);
+	    	Thread.sleep(60000 / wpm);
 	    	
 	    	g.setColor(Color.white);
 	    	g.fillRect(0, 0, width, height);
 	    }
 	}
 
-	
-	public static void demonstratePanelOffset(WordGenerator w, int width, int height, int fontSize, int wpm) throws InterruptedException {
+	/**
+	 *  Instantiates panels and graphics library for displaying text
+	 * @param w
+	 * @param width
+	 * @param height
+	 * @param fontSize
+	 * @param wpm
+	 * @throws InterruptedException
+	 */
+	public static void demonstratePanelOffset(WordGenerator w, int width, int height, int fontSize, 
+			int wpm) throws InterruptedException {
+		
 	    DrawingPanel panel = new DrawingPanel(width, height);
 	    Font f = new Font("Courier", Font.BOLD, fontSize);
 	    Graphics g = panel.getGraphics();
@@ -50,63 +66,74 @@ public class SpeedReader {
 	    }
 	}
 	
-	public static void focusLetter (Graphics g, String s, int width, int height, int wpm, int sW) throws InterruptedException {
+	/**
+	 *  Locates, center, and changes color of focus letter with each word 
+	 * @param g
+	 * @param s
+	 * @param width
+	 * @param height
+	 * @param wpm
+	 * @param sW
+	 * @throws InterruptedException
+	 */
+	public static void focusLetter (Graphics g, String s, int width, int height, int wpm, int sW) 
+			throws InterruptedException {
+		
 		int stringLength = s.length();
 		int charWidth = sW / stringLength; 
-		char c = s.charAt( stringLength-1 );
+		char c = s.charAt(stringLength - 1);
 		
 		if ( stringLength < 2 ){
 	    	g.setColor(Color.red); 
-	    	g.drawString(s.substring(0), (width/2)-(charWidth/2), height/2-20);	
+	    	g.drawString(s.substring(0), (width / 2) - (charWidth / 2), (height / 2) - 20);	
 	    	
 		} else if ( stringLength < 6 ){
 			g.setColor(Color.black);
-	    	g.drawString(s.substring(0, 1), (width/2)-charWidth, height/2-20);
+	    	g.drawString(s.substring(0, 1), (width / 2) - charWidth, (height / 2) - 20);
 	    	g.setColor(Color.red);
-	    	g.drawString(s.substring(1, 2), (width/2), height/2-20);
+	    	g.drawString(s.substring(1, 2), (width / 2), (height / 2) - 20);
 	    	
 	    	if (stringLength > 2){
 	    		g.setColor(Color.black);
-		    	g.drawString(s.substring(2), (width/2)+charWidth, height/2-20);	
+		    	g.drawString(s.substring(2), (width / 2) + charWidth, (height / 2) - 20);	
 	    	}
 		} else if ( stringLength < 10 ){
 			g.setColor(Color.black);
-	    	g.drawString(s.substring(0, 2), (width/2)-charWidth*2, height/2-20);	
+	    	g.drawString(s.substring(0, 2), (width / 2) - (charWidth * 2), (height / 2) - 20);	
 	    	g.setColor(Color.red);
-	    	g.drawString(s.substring(2, 3), (width/2), height/2-20);
+	    	g.drawString(s.substring(2, 3), (width / 2), (height / 2) - 20);
 	    	g.setColor(Color.black);
-	    	g.drawString(s.substring(3), (width/2)+charWidth, height/2-20);	
+	    	g.drawString(s.substring(3), (width / 2) + charWidth, (height / 2) - 20);	
 	    	
 		} else if ( stringLength < 13 ){
 			g.setColor(Color.black);
-	    	g.drawString(s.substring(0, 3), (width/2)-charWidth*3, height/2-20);	
+	    	g.drawString(s.substring(0, 3), (width / 2) - (charWidth * 3), (height / 2) - 20);	
 	    	g.setColor(Color.red);
-	    	g.drawString(s.substring(3, 4), (width/2), height/2-20);
+	    	g.drawString(s.substring(3, 4), (width / 2), (height / 2) - 20);
 	    	g.setColor(Color.black);
-	    	g.drawString(s.substring(4), (width/2)+charWidth, height/2-20);	
+	    	g.drawString(s.substring(4), (width / 2) + charWidth, (height / 2) - 20);	
 	    	
 		} else {
 			g.setColor(Color.black);
-	    	g.drawString(s.substring(0, 4), (width/2)-charWidth*4, height/2-20);	
+	    	g.drawString(s.substring(0, 4), (width / 2) - (charWidth * 4), (height / 2) - 20);	
 	    	g.setColor(Color.red);
-	    	g.drawString(s.substring(4, 5), (width/2), height/2-20);
+	    	g.drawString(s.substring(4, 5), (width / 2), (height / 2) - 20);
 	    	g.setColor(Color.black);
-	    	g.drawString(s.substring(5), (width/2)+charWidth, height/2-20);	
+	    	g.drawString(s.substring(5), (width / 2) + charWidth, (height / 2) - 20);	
 	    	
 		}
 		
 		if ( c == '.' || c == '!' || c == '?' || c == ','){
-			Thread.sleep((long) (60000/wpm*1.75));
+			Thread.sleep((long) ((60000 / wpm) * 1.75));
 		}
 		else {
-			Thread.sleep(60000/wpm);
+			Thread.sleep(60000 / wpm);
 		}
 		
 		g.setColor(Color.white);
     	g.fillRect(0, 0, width, height);
 	}
 	
-
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		// TODO Auto-generated method stub
 
